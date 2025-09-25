56ed59818708dedb0f19ca1c3443d1d2741c66e7
TasosPat <tasos.pat@outlook.com>
2025-09-25 19:37:07 +0100

    env directory corrected

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
 const ENV = process.env.NODE_ENV || 'development';
-dotenv.config({ path: `${__dirname}/../.env.${ENV}` });
+dotenv.config({ path: `${__dirname}/../../.env.${ENV}` });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');

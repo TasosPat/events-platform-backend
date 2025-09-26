d336609f49402b04344952935cab9d25dd638aa9
TasosPat <tasos.pat@outlook.com>
2025-09-26 20:37:18 +0100

    minor correction on empty attendance models

--- src/models/attendanceModels.ts
+++ src/models/attendanceModels.ts
@@ -1,10 +1,10 @@
 import db from '../db/db'
 import { Attendance, NewAttendance } from "../types";
 
-export async function fetchAttendancesByEvent(): Promise<Attendance[]> {
+export async function fetchAttendancesByEvent(event_id: number): Promise<Attendance[]> {
     return [];
 }
 
-export async function fetchAttendancesByUser(attendanceID: number): Promise<Attendance | null> {
+export async function fetchAttendancesByUser(user_id: number): Promise<Attendance[] | null> {
     return null;
 }

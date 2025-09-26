45b949c93bfd917e10819df6c90aea4ed67a3e3e
TasosPat <tasos.pat@outlook.com>
2025-09-26 10:32:55 +0100

    creation of central types file for all interfaces

---
+++ src/types/index.ts
@@ -0,0 +1,30 @@
+export interface NewUser {
+    name: string;
+    role: "user" | "staff";
+    description?: string;
+  }
+
+  export interface User extends NewUser {
+    id: number;
+  }
+
+export interface NewEvent {
+    title: string;
+    description: string;
+    date: string; // ISO format (YYYY-MM-DD)
+    location: string;
+    price?: number; // optional, since some events are free
+  }
+
+  export interface Event extends NewEvent {
+    id: number;
+  }
+
+export interface NewAttendance {
+    event_id: number;
+    user_id: number;
+  }
+  
+  export interface Attendance extends NewAttendance {
+    id: number;
+  }

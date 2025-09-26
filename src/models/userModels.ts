4e6dbdecca2dc0e89c5923832776aafeb42ec80a
TasosPat <tasos.pat@outlook.com>
2025-09-26 20:36:34 +0100

    user models written

--- src/models/userModels.ts
+++ src/models/userModels.ts
@@ -2,9 +2,23 @@ import db from '../db/db'
 import { User, NewUser } from "../types";
 
 export async function fetchUsers(): Promise<User[]> {
-    return [];
+    const query = `SELECT * FROM users ORDER BY user_id ASC`;
+    const { rows } = await db.query(query);
+    return rows;
 }
 
-export async function fetchUserByID(userID: number): Promise<User | null> {
-    return null;
+export async function fetchUserByID(user_id: number): Promise<User | null> {
+    if(!user_id) {
+        return null;
+    }
+    const query = `SELECT * FROM users WHERE user_id=$1`;
+    const args = [user_id];
+    const { rows } = await db.query<User>(query, args);
+    if (!rows[0]) {
+        throw {
+          status: 404,
+          msg: `No user found for user_id: ${user_id}`,
+        };
+      }
+    return rows[0];
 }

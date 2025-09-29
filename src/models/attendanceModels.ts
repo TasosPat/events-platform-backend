fee594c95ddba37090f49c3b6e8c7dd99830b239
TasosPat <tasos.pat@outlook.com>
2025-09-29 11:27:32 +0100

    wrote attendance models

--- src/models/attendanceModels.ts
+++ src/models/attendanceModels.ts
@@ -1,10 +1,16 @@
 import db from '../db/db'
-import { Attendance, NewAttendance } from "../types";
+import { User } from "../types";
 
-export async function fetchAttendancesByEvent(event_id: number): Promise<Attendance[]> {
-    return [];
+export async function fetchAttendancesByEvent(event_id: number): Promise<User[]> {
+    const query = `SELECT u.* FROM attendances a JOIN users u ON a.user_id = u.user_id WHERE a.event_id = $1`;
+    const args = [event_id];
+    const { rows } = await db.query<User>(query, args);
+    return rows;
 }
 
-export async function fetchAttendancesByUser(user_id: number): Promise<Attendance[] | null> {
-    return null;
+export async function fetchAttendancesByUser(user_id: number): Promise<Event[]> {
+    const query = `SELECT e.* FROM attendances a JOIN events e ON a.event_id = e.event_id WHERE a.user_id = $1`;
+    const args = [user_id];
+    const { rows } = await db.query<Event>(query, args);
+    return rows;
 }

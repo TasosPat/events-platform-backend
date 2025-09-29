113bfc8e6db475db77a43a8a33727ea2bbec2580
TasosPat <tasos.pat@outlook.com>
2025-09-29 12:06:58 +0100

    user Controllers written

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -3,7 +3,8 @@ import { fetchUsers, fetchUserByID } from "../models/userModels";
 
 export async function getUsers(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
-        
+        const users = await fetchUsers();
+        res.status(200).json(users);
     } catch (err) {
         next(err);
     } 
@@ -11,7 +12,9 @@ export async function getUsers(req: Request, res: Response, next: NextFunction):
 
 export async function getUserByID(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
-        
+        const { user_id } = req.params;
+        const user = await fetchUserByID(Number(user_id));
+        res.status(200).json(user);
     } catch (err) {
         next(err);
     } 

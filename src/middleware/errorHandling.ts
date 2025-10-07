c54b6d7b0c0fc1e2e7375424749c363c6a88de6e
TasosPat <tasos.pat@outlook.com>
2025-10-07 20:34:59 +0100

    corrected error handling by adding err.message

--- src/middleware/errorHandling.ts
+++ src/middleware/errorHandling.ts
@@ -2,8 +2,10 @@ import { Request, Response, NextFunction } from "express";
 
 export function errorHandler(err: any, req: Request, res: Response, next: NextFunction)
 {
+
+  console.error("🔥 Error caught by middleware:", err);
     const status = err.status || 500;
-    const msg = err.msg || "Internal server error";
+    const msg = err.message || err.msg || "Internal server error";
     res.status(status).json({ msg });
   };
   

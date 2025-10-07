e004f1f3418b513f976f3bd16d2ed539e4816cad
TasosPat <tasos.pat@outlook.com>
2025-10-07 20:34:16 +0100

    added console.log to firebase initialisation

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,8 +1,10 @@
 import admin from "firebase-admin";
-import serviceAccount from "../../firebase-service-account.json"
+import serviceAccount from "../../firebase-service-account.json";
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
 });
+console.log("Firebase initialized ✅");
+
 
 export default admin;

63a744958dad4e08274a2a9a495cbfccd08ee5f4
TasosPat <tasos.pat@outlook.com>
2025-10-08 15:22:51 +0100

    changed firebase auth function to connect local db user details to authenticated firebase user via uid

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import db from '../db/db';
 import { AuthenticatedRequest } from "../types/index"
 import admin from "../config/firebase";
 
@@ -20,11 +21,36 @@ export async function authenticate(req: AuthenticatedRequest, res: Response, nex
     if (!decoded.email) {
         return res.status(400).json({ msg: "Email is missing in token" });
       }
-    req.user = {
+      
+      const result = await db.query('SELECT * FROM users WHERE uid = $1', [decoded.uid]);
+      const dbUser = result.rows[0];
+      
+      if (!dbUser) {
+        // console.log('User not found, allowing POST request to create user');
+       
+        // If the request is a POST to create a new user, proceed
+        // console.log(req.method, req.originalUrl);
+        if (req.method === 'POST' && req.originalUrl === '/api/users') {
+          req.user = {
+            uid: decoded.uid,
+            email: decoded.email,
+            role: "user",
+          };
+          next();
+          return;
+        }
+
+        // Otherwise, block access
+        res.status(404).json({ msg: 'User not found, please register first' });
+        return;
+      }
+      req.user = {
         uid: decoded.uid,
         email: decoded.email,
-        role: decoded.role || "user", // pull role from your DB if you store it separately
+        role: dbUser.role,
+        dbUser
       };
+    
     next();
   } catch (err) {
     return res.status(401).json({ msg: "Invalid token" });

2d2d062e4e6a6b47fe3b05fb9eb045c7eb99d993
TasosPat <tasos.pat@outlook.com>
2025-10-17 23:52:03 +0100

    deleted unnecessary file

--- src/utils/roles.ts
+++
@@ -1,5 +0,0 @@
-export function getRoleForEmail(email?: string): "staff" | "user" {
-    const staffEmails = process.env.STAFF_EMAILS?.split(",").map(e => e.trim()) ?? [];
-    return staffEmails.includes(email ?? "") ? "staff" : "user";
-  }
-  

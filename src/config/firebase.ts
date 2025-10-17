e5892a92671b96108c4f5806708ab000c463e74c
TasosPat <tasos.pat@outlook.com>
2025-10-17 23:56:53 +0100

    added condition to prevent real firebase initialistaion during tests

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,10 +1,11 @@
 import admin from "firebase-admin";
 import serviceAccount from "../../firebase-service-account.json";
 
+if (process.env.NODE_ENV !== "test") {
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
 });
 console.log("Firebase initialized ✅");
-
+}
 
 export default admin;
